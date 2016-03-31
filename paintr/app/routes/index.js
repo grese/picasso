ef9d42354c5a10450daa884057e14eceece2e7cd
grese <grese@yahoo-inc.com>
2016-03-31 12:54:35 -0700

    changed index route to return a single image model

--- paintr/app/routes/index.js
+++ paintr/app/routes/index.js
@@ -3,19 +3,21 @@ import Em from 'ember';
 export default Em.Route.extend({
     login: Em.inject.service(),
     model: function() {
-        return this.store.findAll('image');
+        return this.store.createRecord('image', {
+            user_id: this.get('login.user.id'),
+            user: this.get('login.user')
+        });
     },
 
     actions: {
         createImage: function(params) {
             params = params || {};
 
-            var image = this.store.createRecord('image', {
-                user: this.get('login.user'),
-                title: params.title,
-                data_uri: params.data_uri
+            this.get('currentModel').setProperties({
+                data_uri: params.data_uri,
+                title: params.title
             });
-            image.save().then(function(resp) {
+            this.get('currentModel').save().then(function(resp) {
                 Em.Logger.debug('THE IMAGE RESPONSE: ', resp);
             });
         }
