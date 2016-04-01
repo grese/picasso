d995075adc3905272e1a4eced02c6ad247c3e959
grese <johngrese@yahoo.com>
2016-04-01 00:06:45 -0700

    better data, and fix bug

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,16 +1,38 @@
 
 # Create mock records for development:
 
-user1 = User.create({email: 'johngrese@me.com', username: 'thedude', password: 'schroeder', confirmation: true})
-user1.images.create({title: 'Oh, the usual. I bowl. Drive around. The occasional acid flashback.', filename: 'sample-4.png'})
+user1 = User.create({email: 'john@thebeatles.com', username: 'johnlennon', password: 'password', confirmation: true})
+user1.images.create([
+                        {title: 'I Am the Walrus', filename: 'sample-1.png'},
+                        {title: 'Help!', filename: 'sample-2.png'},
+                        {title: 'Come Together', filename: 'sample-3.png'}
+                    ])
 
-user2 = User.create({email: 'frankgrese@yahoo.com', username: 'frank', password: 'abcdefg', confirmation: true})
-user2.images.create({title: 'helleau whirrled', filename: 'sample-1.png'})
+user2 = User.create({email: 'paul@thebeatles.com', username: 'paulmccartney', password: 'password', confirmation: true})
+user2.images.create([
+                        {title: 'Helter Skelter', filename: 'sample-4.png'},
+                        {title: 'Something', filename: 'sample-5.png'},
+                        {title: 'Its All Too Much', filename: 'sample-6.png'}
+                    ])
 
-user3 = User.create({email: 'katiegrese@yahoo.com', username: 'katie', password: 'abcdefg', confirmation: true})
-user3.images.create({title: 'look at me', filename: 'sample-3.png'})
+user3 = User.create({email: 'george@thebeatles.com', username: 'georgeharrison', password: 'password', confirmation: true})
+user3.images.create([
+                        {title: 'Taxman', filename: 'sample-7.png'},
+                        {title: 'Hey Jude', filename: 'sample-8.png'},
+                        {title: 'A Day In The Life', filename: 'sample-9.png'}
+                    ])
 
-user4 = User.create({email: 'schroeder@grese.me', username: 'schroeder', password: 'abcdefg', confirmation: true})
-user4.images.create({title: 'givadogabone', filename: 'sample-2.png'})
+user4 = User.create({email: 'ringo@thebeatles.com', username: 'ringostarr', password: 'password', confirmation: true})
+user4.images.create([
+                        {title: 'Octopus\'s Garden', filename: 'sample-10.png'},
+                        {title: 'Revolution', filename: 'sample-11.png'},
+                        {title: 'Hello Goodbye', filename: 'sample-12.png'}
+                    ])
 
+me = User.create({email: 'john@grese.me', username: 'grese', password: 'password', confirmation: true})
+me.images.create([
+                        {title: 'Beatle Mania', filename: 'sample-13.png'},
+                        {title: 'Quarrymen', filename: 'sample-14.png'},
+                        {title: 'Yoko', filename: 'sample-15.png'}
+                    ])
 
