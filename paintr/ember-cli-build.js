34bbf665d952cf9518e9b4fb83bd3310f4c1c500
grese <grese@yahoo-inc.com>
2016-03-31 17:02:16 -0700

    polishing the navbar

--- paintr/ember-cli-build.js
+++ paintr/ember-cli-build.js
@@ -4,11 +4,15 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
     var app = new EmberApp(defaults, {
-        // Add options here
     });
 
+    // import bootstrap JS...
+    app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+
+    // import paintr...
     app.import('vendor/paintr/paintr.js');
     app.import('vendor/paintr/paintr.css');
+
 
     return app.toTree();
 };
